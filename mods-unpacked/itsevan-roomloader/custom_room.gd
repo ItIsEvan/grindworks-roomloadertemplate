e903b919b611e188f9f2338e1835b40910e57b03
ItIsEvan <113439863+ItIsEvan@users.noreply.github.com>
2025-11-20 07:19:31 -0500

    Preliminary fix for 1.2+

--- mods-unpacked/itsevan-roomloader/custom_room.gd
+++ mods-unpacked/itsevan-roomloader/custom_room.gd
@@ -14,30 +14,32 @@ enum RoomDept {
 	CASH,
 	LAW,
 	BOSS,
+	MOLTEN,
 }
 @export var room_dept := RoomDept.SELL
 
-const ROOM_MAPPING : Dictionary[RoomDept, String] = {
-	RoomDept.SELL : "res://scenes/game_floor/department_floors/rooms_sell.tres",
-	RoomDept.CASH : "res://scenes/game_floor/department_floors/rooms_cash.tres",
-	RoomDept.LAW : "res://scenes/game_floor/department_floors/rooms_law.tres",
-	RoomDept.BOSS : "res://scenes/game_floor/department_floors/rooms_boss.tres"
+const ROOM_MAPPING: Dictionary[RoomDept, String] = {
+	RoomDept.SELL: "res://scenes/game_floor/department_floors/rooms_sell.tres",
+	RoomDept.CASH: "res://scenes/game_floor/department_floors/rooms_cash.tres",
+	RoomDept.LAW: "res://scenes/game_floor/department_floors/rooms_law.tres",
+	RoomDept.BOSS: "res://scenes/game_floor/department_floors/rooms_boss.tres",
+	RoomDept.MOLTEN: "res://scenes/game_floor/department_floors/rooms_molten.tres",
 }
 
-const ROOM_ARRAYS : Dictionary[RoomType, String] = {
-	RoomType.BATTLE : "battle_rooms",
-	RoomType.OBSTACLE : "obstacle_rooms",
-	RoomType.SPECIAL : "special_rooms",
-	RoomType.BOSS : "final_rooms",
+const ROOM_ARRAYS: Dictionary[RoomType, String] = {
+	RoomType.BATTLE: "battle_rooms",
+	RoomType.OBSTACLE: "obstacle_rooms",
+	RoomType.SPECIAL: "special_rooms",
+	RoomType.BOSS: "final_rooms",
 }
 
 func setup() -> void:
 	Util.s_floor_started.connect(inject_room)
 
-func inject_room(game_floor : GameFloor) -> void:
-	var department_floor : DepartmentFloor = game_floor.floor_variant.floor_type
+func inject_room(game_floor: GameFloor) -> void:
+	var department_floor: DepartmentFloor = game_floor.floor_variant.floor_type
 	if not department_floor.resource_path == ROOM_MAPPING[room_dept]: return
-	var room_array : Array[FacilityRoom] = department_floor.get(get_array_name())
+	var room_array: Array[FacilityRoom] = department_floor.get(get_array_name())
 	if not self in room_array:
 		room_array.append(self)
 		department_floor.set(get_array_name(), room_array)
